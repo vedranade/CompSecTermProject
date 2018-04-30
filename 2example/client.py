c1770b30e7ca78c8e668df3b9edafbc96593bb8c
Ved Ranade <ved@sager.xyz>
2018-04-30 16:03:32 -0400

    Original code.

---
+++ 2example/client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+def main():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = "127.0.0.1"
+    port = 8888
+
+    try:
+        soc.connect((host, port))
+    except:
+        print("Connection error")
+        sys.exit()
+
+    #If connection successful:
+    #vname = raw_input("Enter name: ")
+    #vregnum = raw_input("Enter registration number: ")
+    print("Enter 'quit' to exit")
+    message = input(" -> ")
+
+    while message != 'quit':
+        soc.sendall(message.encode("utf8"))
+        if soc.recv(5120).decode("utf8") == "-":
+            pass        # null operation
+
+        message = input(" -> ")
+
+    soc.send(b'--quit--')
+
+if __name__ == "__main__":
+    main()
